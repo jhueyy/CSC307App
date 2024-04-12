0b78e3d272a26899adfb0e6aa39d3e9451022fe7
Jake Huey <jakehuey@gmail.com>
2024-04-11 21:30:40 -0700

    finished app!

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -3,11 +3,24 @@
 //all that data will pass to the MyApp state (feeding the list of characters), which will then update Table. 
 import React, { useState } from "react";
 
-function Form() {
+function Form(props) {
   const [person, setPerson] = useState({
     name: "",
     job: ""
   });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    if (name === "job")
+      setPerson({ name: person["name"], job: value });
+    else setPerson({ name: value, job: person["job"] });
+  }
+
+  function submitForm() {
+    props.handleSubmit(person);
+    setPerson({ name: "", job: "" });
+  }
+
   return (
     <form>
       <label htmlFor="name">Name</label>
@@ -26,17 +39,15 @@ function Form() {
         value={person.job}
         onChange={handleChange}
       />
+      <input type="button" value="Submit" onClick={submitForm} />
     </form>
   );
   
 }
 
-function handleChange(event) {
-    const { name, value } = event.target;
-    if (name === "job")
-      setPerson({ name: person["name"], job: value });
-    else setPerson({ name: value, job: person["job"] });
-  }
+
+
+
 
 
 export default Form;
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,15 +1,11 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
+
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-      {
-        name: "Charlie",
-        job: "Janitor"// the rest of the data
-
-      }
-    ]);
+    const [characters, setCharacters] = useState([]);
   
     function removeOneCharacter(index) {
       const updated = characters.filter((character, i) => {
@@ -17,16 +13,26 @@ function MyApp() {
       });
       setCharacters(updated);
     }
+    function updateList(person) {
+        setCharacters([...characters, person]);
+    }
+
+      
     return (
         <div className="container">
-          <Table
-            characterData={characters}
-            removeCharacter={removeOneCharacter}
-          />
-        </div>
+    <Table
+      characterData={characters}
+      removeCharacter={removeOneCharacter}
+    />
+    <Form 
+    handleSubmit={updateList}
+    />
+  </div>
       );
       
   }
+
+  
   
 
 export default MyApp;
