73df5b5b1ad3059e4425a9318f2e3aa7f49a1663
Jake Huey <jakehuey@gmail.com>
2024-04-15 22:34:43 -0700

    added get user by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,6 +34,20 @@ const users = {
   ]
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+
 const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
